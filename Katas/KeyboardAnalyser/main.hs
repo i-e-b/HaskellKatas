2ec91ad85bc9e915adabac41c9ac5dbc367b28ab
Iain Ballard <iain.ballard@7digital.com>
2013-06-11 16:59:17 +0100

    Mostly working now, very slightly less ugly

--- Katas/KeyboardAnalyser/main.hs
+++ Katas/KeyboardAnalyser/main.hs
@@ -17,7 +17,10 @@ main = do
 	str <- getContents
 	putStrLn . analyseString $ str
 
-analyseString x = printAnalysis . (analyse theKeyboard) $ x
+analyseString x = concat
+	[printAnalysis . (analyse theKeyboard) $ x 
+	, "\r\n"
+	, printStatistics . statistics . (analyse theKeyboard) $ x]
 	
 data Side = L | R deriving (Show, Eq)
 type Column = Integer
@@ -27,10 +30,16 @@ data Statistics = Statistics { left::Int, right::Int, collisions::Int } deriving
 
 type Analysis = [[KeyPosition]]
 type Keyboard = [KeyPosition]
+{-
+ QCK  DL.
+ARST  HNEI
+GPB_  FJOY
+ XUM  WVZ
+-}
 theKeyboard =
-	[             ('Q', 1, L), ('C', 2, L), ('K', 3, L),     ('D', 4, R), ('L', 5, R), ('.', 6, R)
+	[             ('Q', 1, L), ('C', 2, L), ('K', 3, L),     ('D', 4, R), ('L', 5, R), ('.', 6, R), (',', 6, R) -- dot and comma on same key
 	,('A', 0, L), ('R', 1, L), ('S', 2, L), ('T', 3, L),     ('H', 4, R), ('N', 5, R), ('E', 6, R), ('I', 7, R)
-	,('B', 0, L), ('F', 1, L), ('P', 2, L), ('G', 3, L),     (' ', 4, R), ('J', 5, R), ('O', 6, R), ('Y', 7, R)
+	,('G', 0, L), ('P', 1, L), ('B', 2, L), (' ', 3, L),     ('F', 4, R), ('J', 5, R), ('O', 6, R), ('Y', 7, R)
 	,             ('X', 1, L), ('U', 2, L), ('M', 3, L),     ('W', 4, R), ('V', 5, R), ('Z', 6, R)]
 
 {- Targets:
@@ -43,17 +52,37 @@ theKeyboard =
 printAnalysis :: Analysis -> String
 printAnalysis anz = concat (map inner anz)
 	where
-		dEnd x = if (length x) > 1 then "]" else ""
-		dStart x = if (length x) > 1 then "[" else ""
-		inner x = (dStart x) ++ (map caseLR x) ++ dEnd x
+		col str x = if (length x) > 1 then str else ""
+		inner x = concat [ f x | f <- [col "[", caseLR, col "]"]]
+		caseLR  = map (\(chr, col, side) -> if (side == L) then (toLower chr) else (toUpper chr))
 
-caseLR :: KeyPosition -> Char
-caseLR (chr, col, side) = if (side == L) then (toLower chr) else (toUpper chr)
+printStatistics :: Statistics -> String
+printStatistics x = "Left keys: " ++ (lefts x) ++ "; Right keys: " ++ (rights x) ++ "; Collisions: " ++ (collides x)
+	where
+		lefts (Statistics ls rs cl) = show ls
+		rights (Statistics ls rs cl) = show rs
+		collides (Statistics ls rs cl) = show cl
 		
 -- Analyse a string against a keyboard.
 analyse :: Keyboard -> String -> Analysis
 analyse k s = groupedKeys . (mapKeys k) $ singles s
 
+-- Get usage statistics
+statistics :: Analysis -> Statistics
+statistics = (foldl (addstat) (Statistics 0 0 0)) . statlist
+	where
+		addstat (Statistics lA rA cA) (Statistics lB rB cB) = Statistics (lA + lB) (rA + rB) (cA + cB)
+
+statlist :: [[KeyPosition]] -> [Statistics]
+statlist x = (concat [map (leftOrRight) y | y <- x]) ++ (map collides x)
+	where
+		leftOrRight keypos = Statistics (isLeftSide keypos) (isRightSide keypos) 0
+		collides grp = Statistics 0 0 ((length grp) - 1)
+
+isLeftSide :: KeyPosition -> Int
+isLeftSide (_, _, side) = if (side == L) then 1 else 0
+isRightSide x = 1 - (isLeftSide x)
+		
 -- string with double-letters removed
 singles :: String -> String
 singles str = map (\x -> x !! 0) (group str)
