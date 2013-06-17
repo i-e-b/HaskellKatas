999edc40fdc7cc10db2ddf50a6715492fec91040
Iain Ballard <iain.ballard@7digital.com>
2013-06-17 18:08:18 +0100

    Checkout kata: trivial in Haskell

--- Katas/Checkout/main.hs
+++ Katas/Checkout/main.hs
@@ -8,20 +8,39 @@ Item     Price     Offer
  D       15
  
 Accept items in any order, apply matching offers
-
-Sample assertions:
-
-"" = 0
-"A" = 50
-"AB" = 80
-"CDBA" = 115
-"AA" = 100
-"AAA" = 130
-"AAABB" = 150
-"ACAABCB" = 190
-
 -}
 
 import Data.List
 import Test.HUnit
 
+-- Unit tests
+
+tests = TestList [ "empty"		~: (0)				~=? (priceOf "")
+                 , "A"			~: (50)				~=? (priceOf "A")
+                 , "AB"			~: (80)				~=? (priceOf "AB")
+                 , "CDBA"		~: (115)			~=? (priceOf "CDBA")
+                 , "AA"			~: (100)			~=? (priceOf "AA")
+                 , "AAA"		~: (130)			~=? (priceOf "AAA")
+                 , "AAABB"		~: (175)			~=? (priceOf "AAABB")
+                 , "ACAABCB"	~: (215)			~=? (priceOf "ACAABCB")
+                 ]
+--
+-- On execute, run tests.
+main = do
+	runTestTT tests
+	
+-- Implementation
+
+priceOf :: String -> Int
+priceOf str = sum $ map (deals) (group $ sort str)
+
+deals :: String -> Int
+deals "AAA" = 130
+deals "BB" = 45
+deals other = sum $ map price other
+	where
+		price 'A' = 50
+		price 'B' = 30
+		price 'C' = 20
+		price 'D' = 15
+		price _ = undefined
