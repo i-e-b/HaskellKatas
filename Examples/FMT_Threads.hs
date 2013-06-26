f0265128edce558507e86072a6394d67af0085ec
i-e-b <ieb@me.com>
2013-06-26 22:35:13 +0100

    FMT threads compiling

--- Examples/FMT_Threads.hs
+++ Examples/FMT_Threads.hs
@@ -4,10 +4,13 @@
 --   http://www.haskellforall.com/2013/06/from-zero-to-cooperative-threads-in-33.html
 
 import Control.Monad.Trans.Free  -- from the `free` package
+import Control.Monad.Trans
 import Control.Monad
 import Data.Sequence -- Queue with O(1) head and tail operations
 
 -- Using: --
+main = roundRobin mainThread :: IO ()
+
 mainThread :: Thread IO ()
 mainThread = do
     lift $ putStrLn "Forking thread #1"
@@ -72,4 +75,21 @@ roundRobin t = go (singleton t)  -- Begin with a single thread
                 Free  Done        -> go ts'
                 Pure  _           -> go ts'
 --
+{-data FreeF f a x = Pure a | Free (f x)
+
+newtype FreeT f m a =
+    FreeT { runFreeT :: m (FreeF f a (FreeT f m a)) }
+
+instance (Functor f, Monad m) => Monad (FreeT f m) where
+    return a = FreeT (return (Pure a))
+    FreeT m >>= f = FreeT $ m >>= \v -> case v of
+        Pure a -> runFreeT (f a)
+        Free w -> return (Free (fmap (>>= f) w))
+
+instance MonadTrans (FreeT f) where
+    lift = FreeT . liftM Pure
+
+liftF :: (Functor f, Monad m) => f r -> FreeT f m r
+liftF x = FreeT (return (Free (fmap return x)))
+-}
 
