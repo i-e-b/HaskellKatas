3d133f5932b876b58be34872ac1f8969f325b08f
Iain Ballard <iain.ballard@7digital.com>
2013-06-27 14:13:06 +0100

    returned bellringing to spec, simpler and cleaner romans

--- Katas/Bellringing/main.hs
+++ Katas/Bellringing/main.hs
@@ -8,7 +8,8 @@ import Test.HUnit
 import Data.List
 
 -- Unit tests
-{-
+
+bellsAtPosition x = bells !! x
 tests = TestList [ "initital position 1"	~: ([1])				~=? (take 1 $ bellsAtPosition 0)
                  , "initital position 2"	~: ([2])				~=? (take 1 $ bellsAtPosition 1)
 				 , "initial round"			~: ([1,2,3,4,5,6,7,8])	~=? (bellsAtPosition 0)
@@ -18,17 +19,12 @@ tests = TestList [ "initital position 1"	~: ([1])				~=? (take 1 $ bellsAtPositi
 				 , "round 16"				~: ([1,3,5,2,7,4,8,6])	~=? (bellsAtPosition 16)
 				 , "round 112"				~: ([1,2,3,4,5,6,7,8])	~=? (bellsAtPosition 112)
                  ]
--}
-tests = TestList [ "initial round"			~: (["Sam", "Jane", "Tom", "Bob", "Steve", "James", "Harriet", "Sophie"])	~=? (bellsAtPosition 0)
-				 , "round 16"				~: (["Sam","Tom","Steve","Jane","Harriet","Bob","Sophie","James"])	~=? (bellsAtPosition 16)
-                 ]
+
 main = do
 	runTestTT tests
 
 -- Settings
-
---start = [1..8]										-- The initial list of bell pullers
-start = ["Sam", "Jane", "Tom", "Bob", "Steve", "James", "Harriet", "Sophie"]
+start = [1..8]										-- The initial list of bell pullers
 pattern = (7 `_of` [[], [1,8]]  ) ++ [[], [1,2]]	-- 'hold' patterns. 1-based indexes of pullers to stay put
 
 -- Implementation
@@ -58,6 +54,5 @@ bellLoop (pat:xs) current =
 	in  [next] ++ (bellLoop xs next)
 
 -- Little helpers
-bellsAtPosition x = bells !! x
 _of x = concat . replicate x
 
--- Katas/Romans/main.hs
+++ Katas/Romans/main.hs
@@ -1,31 +1,26 @@
-import qualified Data.List as T
+import Data.List
 
 main = do
 	putStrLn "Give me a natural number"
 	numinp <- getLine
 	let n = read numinp :: Int
 	putStrLn . romanise $ n
+--
+
+-- Mapping of descending integer values and their RN equivalents.
+romanMapping :: [(Int, String)]
+romanMapping = 
+	[ (1000, "M"),  (500, "D"),  (100, "C"),  (90, "XC")
+	, (50,   "L"),  (40,  "XL"), (10,  "X"),  (9,  "IX")
+	, (5,    "V"),  (4,   "IV"), (1,   "I")]
 
 romanise :: Int -> String
-romanise = optimise . numerals [(1000, 'M'), (500, 'D'),(100, 'C'),(50, 'L'),(10, 'X'),(5, 'V'),(1, 'I')] ""
+romanise = numerals romanMapping ""
 
-numerals :: [(Int,Char)] -> String -> Int -> String
+-- decrement the input number by each reducing scale while the input is >= the scale
+-- [Scale] -> starting string -> input number -> output string
+numerals :: [(Int,String)] -> String -> Int -> String
 numerals ((scale, chr):xs) s i
-	| i >= scale = numerals ((scale,chr):xs) (s ++ [chr]) (i - scale)
+	| i >= scale = numerals ((scale,chr):xs) (s ++ chr) (i - scale)
 	| otherwise  = numerals xs s i
 numerals [] s i = s
-
-optimise :: String -> String
-optimise inp = T.concat . optimiseGroups . T.group $ inp
-
-optimiseGroups :: [String] -> [String]
-optimiseGroups x = map subopt x 
-
-subopt :: String -> String
-subopt "IIII" = "IV"
-subopt "III" = "IIV"
-subopt "XXXX" = "XL"
-subopt "CCCC" = "CD"
-subopt "CCC" = "CCD"
-subopt x = x
-
