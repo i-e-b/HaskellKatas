30987b3ce1cf372667e2adaa7f2eb1cfc1b17232
Iain Ballard <iain.ballard@7digital.com>
2013-07-09 16:15:25 +0100

    Actual reading with HaXml

--- Experiments/Xml/HaXml.hs
+++ Experiments/Xml/HaXml.hs
@@ -9,13 +9,23 @@ import Text.XML.HaXml.Posn
 
 exampleFile = "example.xml"
 
-main = readFile exampleFile >>= putStrLn . processXml .(xmlParse ("error in " ++ exampleFile))
+main = readFile exampleFile >>= putStrLn . testFunc .(xmlParse ("error in " ++ exampleFile))
+
+testFunc = howManyDeals -- should be 12
 
 -- really simple test: get the name of the root element
-processXml :: Document a -> String
-processXml (Document _ _ elem _ ) = elemName elem
+rootElementName :: Document a -> String
+rootElementName (Document _ _ elem _ ) = elemName elem
 	where
 		elemName (Elem (N n) _ _) = n
 		elemName (Elem (QN ns n) _ _) = n
 --
+
+howManyDeals :: Document Posn -> String
+howManyDeals doc = show . length $ allTags "Deal" (rootElemContent doc)
+	where
+		allTags = deep . tag
+
+rootElemContent :: Document Posn -> Content Posn
+rootElemContent (Document _ _ elem _ ) = CElem elem noPos
 
