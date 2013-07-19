7c82ece2cff667c78160b026c2048048e7c10754
Iain Ballard <iain.ballard@7digital.com>
2013-07-19 18:01:10 +0100

    bitset progress

--- Katas/Bloom/BitSet.hs
+++ Katas/Bloom/BitSet.hs
@@ -8,27 +8,42 @@ import Data.List
 
 newtype BitSet = BitSet [Word64] deriving (Show, Eq)
 
+-- empty bitstring, no elements set
 empty :: BitSet
 empty = BitSet []
 
-oneBit :: (Integral a) => a -> BitSet
-oneBit x = BitSet (unfoldr (blocksFor x) 1)
+-- given a bit position (x), return a BitSet with only that bit set
+oneBit :: (Integral a) => a -> [Word64]
+oneBit x = unfoldr (blocksFor x) 1
+	where
+	blocksFor :: (Integral b) => b -> b -> Maybe (Word64, b)
+	blocksFor index block 
+		| block < 1 + (index `div` 64)  = Just (0, (block + 1))
+		| block == 1 + (index `div` 64) = Just (fromIntegral 1 `shiftL` (fromIntegral index `mod` 64), (block + 1))
+		| otherwise = Nothing
 
 
-blocksFor :: (Integral b) => b -> b -> Maybe (Word64, b)
-blocksFor index block 
-	| block < 1 + (index `div` 64)  = Just (0, (block + 1))
-	| block == 1 + (index `div` 64) = Just (fromIntegral 1 `shiftL` (fromIntegral index `mod` 64), (block + 1))
-	| otherwise = Nothing
 
-set :: Ord a => a  -> BitSet -> BitSet
-set x bs = bs
+-- return the input bitstring with bit (x) set, regardless of if it was set before.
+set :: Integral a => a  -> BitSet -> BitSet
+set x (BitSet bs) = BitSet (zipLong 0 (.|.) (oneBit x) bs)
 
+-- returns true if bit (x) of the bitstring is set
 isSet :: Ord a => a  -> BitSet -> Bool
-isSet x bs = False
+isSet x (BitSet bs) = False
 
+-- given a set of positions, returns true if any are set
 anySet :: Ord a => [a] -> BitSet -> Bool
 anySet xs bs = False
 
+-- given a set of positions, returns true iff all are set.
 allSet :: Ord a => [a] -> BitSet -> Bool
 allSet xs bs = False
+
+-- like zipWith, but uses length of longest list, not shortest
+-- if lists are not equal, the id (x) is used as a proxy
+zipLong :: a -> (a -> a -> c) -> [a] -> [a] -> [c]
+zipLong p f (x:xs) (y:ys) = f x y : zipLong p f xs ys
+zipLong p f []     (y:ys) = f p y : zipLong p f [] ys
+zipLong p f (x:xs) []     = f x p : zipLong p f xs []
+zipLong _ _ _ _ = []
