fda12fb4fcc8987c10f6f64613b56dbe1fb916b4
Iain Ballard <iain.ballard@7digital.com>
2013-08-06 17:17:28 +0100

    twiddling

--- Experiments/Chiptunes/main.hs
+++ Experiments/Chiptunes/main.hs
@@ -6,8 +6,27 @@ import qualified Data.ByteString.Lazy as B
 
 main = return choons >>= B.putStr
 
+mapPack :: (Word32 -> Word32) -> ByteString
+mapPack f = repack $ map f seed
+
+soundFunc :: Word32 -> Word32
+
+-- square wave with variable duty
+soundFunc t = if ((t `mod` 100) < 20) then 0 else 255
+
+
+-- simple little tune
+--soundFunc = (\t -> t * (((t `shiftR` 12) .|. (t `shiftR` 8)) .&. (63 .&. (t `shiftR` 4))))
+
+
+
+
+
 choons :: ByteString
-choons = repack $ map (\t -> t * (((t `shiftR` 12) .|. (t `shiftR` 8)) .&. (63 .&. (t `shiftR` 4)))) seed
+choons = mapPack soundFunc
+
+--choons :: ByteString
+--choons = repack $ map (\t -> t * (((t `shiftR` 12) .|. (t `shiftR` 8)) .&. (63 .&. (t `shiftR` 4)))) seed
 
 repack :: [Word32] -> ByteString
 repack x = pack $ map fromIntegral x
