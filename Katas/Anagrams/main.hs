d75ece3fc01385248a5488aabc2285c35d5151bb
Iain Ballard <iain.ballard@7digital.com>
2013-08-08 18:02:51 +0100

    Sketching

--- Katas/Anagrams/main.hs
+++ Katas/Anagrams/main.hs
@@ -1,21 +1,20 @@
--- Anagrams with hashes
--- 
--- The basic idea is to have a dictionary lookup of (word sorted)->(word)
--- then to sort the input and take varying length slices looking for entries in the dict map.
-
+-- Anagrams
 import Data.List
 import Data.Maybe
 import Data.Function
 import Data.Traversable
+import Control.Applicative
 import qualified Data.Map as M
 
 type Dictionary = [String]
 type GroupedDictionary = [(Int, Dictionary)] -- (length of each entry, entries)
 type WordList = [String]
 
+
 data Match = Match { remains :: [Char]             -- the remaining characters of the input. At root, this is the input. At leafs it is []
                    , matches :: M.Map String Match -- map of dictionary word matched to sub-matches
 				   } deriving (Show)
+--
 
 main :: IO ()
 main = do
@@ -25,12 +24,32 @@ main = do
 	putStrLn . unlines $ anagrams (groupedDictionary . words $ content) input
 	
 anagrams :: GroupedDictionary -> String -> WordList
-anagrams dic inpStr = unroll (buildMatchTree dic (crush inpStr))
+anagrams dic inpStr = concat $ unroll (buildMatchTree dic (crush inpStr))
 
-unroll :: Match -> WordList
-unroll = undefined
+-- traverse a match tree and build lists of dictionary word paths
+-- any paths that end in non-empty 'remains' are invalid.
+unroll :: Match -> [WordList]
+unroll = undefined 
+-- Depth-first-search. Where on a leaf, if (deadEnd) then (nothing) else (add to accumulator)
 
+-- something like this?
+accumResults :: [WordList] -> Match -> [WordList]
+accumResults acc m = [ accumResults (acc ++ (peel x)) ...? | x <- matches m, not deadend m ...]
 
+isLeaf :: Match -> Bool
+isLeaf m = M.null (matches m)
+
+-- If true, this is the leaf of a path that is no good
+deadEnd :: Match -> Bool
+deadEnd m = (remains m /= []) && (M.keys (matches m) == []) -- end of the match path, but letters left over.
+
+-- get all the dictionary words at a single match point
+getPointWords :: Match -> [String]
+getPointWords mtc = M.keys $ matches mtc
+
+-- build the match tree. at each level, finds all single dictionary words contained in the 
+--  remaining string, and starts a new match sub tree with the matched word removed from 
+--  the remaining string.
 buildMatchTree :: GroupedDictionary -> String -> Match
 buildMatchTree gDic srcStr = m gDic srcStr
 	where
