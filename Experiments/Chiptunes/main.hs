c6a109009ea86f0ba41f900deee14a7eecf7ba41
i-e-b <ieb@me.com>
2013-08-14 00:14:59 +0100

    too much mucking around with sin approximations

--- Experiments/Chiptunes/main.hs
+++ Experiments/Chiptunes/main.hs
@@ -13,6 +13,13 @@ soundFunc :: Word32 -> Word32
 -- square wave with variable duty
 soundFunc t = if ((t `mod` 100) < 20) then 0 else 255
 
+-- output a sin-like waveform that takes a given number of samples to cycle
+sineLikeWave :: Int -> [Int]
+sineLikeWave rate =
+	let step = (-3.14159265) + (6.28318531 / (fromIntegral rate))
+	    angles = cycle [-3.14159265,step..3.14159265] -- the step size would be the rate
+	in  map (\x -> floor $ 127.0 * (f x)) angles
+	where f x = if (x < 0) then (1.27323954 * x + 0.405284735 * x * x) else (1.27323954 * x - 0.405284735 * x * x)
 
 -- simple little tune
 --soundFunc = (\t -> t * (((t `shiftR` 12) .|. (t `shiftR` 8)) .&. (63 .&. (t `shiftR` 4))))
@@ -33,11 +40,15 @@ noise seed len = (concat . repeat . take len) $ noiseFunc seed
 
 
 choons :: ByteString
-choons = repack bitwiseNoise
+--choons = repack bitwiseNoise
 --choons = mapPack soundFunc
+choons = pack127 $ sineLikeWave 18
 
 --choons :: ByteString
 --choons = repack $ map (\t -> t * (((t `shiftR` 12) .|. (t `shiftR` 8)) .&. (63 .&. (t `shiftR` 4)))) seed
+
+pack127 :: [Int] -> ByteString
+pack127 x = pack $ map (\i -> fromIntegral (i+127))  x
 
 repack :: [Word32] -> ByteString
 repack x = pack $ map fromIntegral x
