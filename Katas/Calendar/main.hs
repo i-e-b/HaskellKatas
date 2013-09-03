bddf6841da5e9423c81f1949023d928d143c5145
i-e-b <ieb@me.com>
2013-09-03 23:17:54 +0100

    Working calendar

--- Katas/Calendar/main.hs
+++ Katas/Calendar/main.hs
@@ -30,26 +30,27 @@ intInput msg = putStrLn msg >> getLine >>= \s -> return (read s :: Int)
 
 
 -- given a year and a screen width, output a display string for the year calendar
-
 calendarFor :: Int -> Int -> String
 calendarFor year width = unlines $ grouped
 	where
 		perRow = width `div` 21
 		grouped = map (\x -> clump year x perRow) [1,(perRow+1)..12]
 
-clump :: Int -> Int -> Int -> String
-clump year start row = unlines . (map (unwords)) . (tposed row) $ blockm year [start..(start+row-1)]
+		clump :: Int -> Int -> Int -> String
+		clump year start row = unlines . (map (unwords)) . (groupIntoColumns row) $ blockm year [start..(start+row-1)]
 
-tposed :: Int -> [[String]] -> [[String]]
-tposed r = transpose . concat . (groupsOf r)
+		groupIntoColumns :: Int -> [[String]] -> [[String]]
+		groupIntoColumns r = transpose . concat . (groupsOf r)
 
-blockm :: Int -> [Int] -> [[String]]
-blockm year months = [ monthToStrings year month | month <- months, month <= 12]
+		blockm :: Int -> [Int] -> [[String]]
+		blockm year months = [ monthToStrings year month | month <- months, month <= 12]
 
 -- given a year and a month, draw a calendar block
 monthToStrings :: Int -> Int -> [String]
-monthToStrings year month = weekLines $ weeksOfMonth year month
+monthToStrings year month = monthTitle : dayCols : (weekLines $ weeksOfMonth year month)
 	where
+		monthTitle = pad (replicate 21 ' ') (monthNames !! (month-1))
+		dayCols = "  " ++ (intercalate "  " dayTitles)
 		weekLines [] = []
 		weekLines wks = concatMap (dayStr) (take 7 wks) : weekLines (drop 7 wks)
 
@@ -64,8 +65,9 @@ weeksOfMonth year month = -- only first week to start with
 	let start = (monthStart year month) - 1
 	    length = (monthLengths year) !! (month - 1)
 	    leader = map (\_ -> Blank) [1..start]
-	    rest = map (\day -> Cell day) [1..length]
-	in leader ++ rest
+	    run = map (\day -> Cell day) [1..length]
+	    ending = map (\_ -> Blank) [(length+start)..42] -- 6 rows of 7 days, the max size of a month
+	in leader ++ run ++ ending
 
 -- list of month lengths for a year
 monthLengths :: Int -> [Int]
