50c0e9b6836d2640d293fac37772ffdabb92fb7d
i-e-b <ieb@me.com>
2014-02-09 21:03:07 +0000

    placeholder for holograms

---
+++ Experiments/Hologram/Main.hs
@@ -0,0 +1,15 @@
+-- A basic experiment with integral interference patterns
+
+{-
+ - Waves are emitted from a source toward both the 'plate' and 'subject'.
+ - reflections from subject arrive at plate.
+ - Difference in phase between direct and reflected wave is summed
+ - (each point in the plate receives waves from every unoccluded point
+ -  of the subject)
+ -
+ - In physical plates, this causes sinc concentric circles, each of which
+ - interferes with others.
+ -
+ - shining a similar set of waves on the plate should cause the subject
+ - to appear in image.
+ -}
